f2b1812ee588d95b30af01b0fcaaa5ed757f4bb9
Eric Hagen <ehagen@ischool.berkeley.edu>
2016-09-22 11:23:16 -0700

    submitting hw 3

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,13 +1,31 @@
 $(document).ready(
     $("#new-item").on('click', function() {
         // once the document loads, create new item with this function
+        //alert("suckit");
+        var user_input = $('#todo-item-input').val();
+
+        // append new task to the top of to do list
+        $('#todolistlist').append("<li>" + user_input + "<button> Add to Completed List </button></li>");
+        
+        // clear input box
+        $("#todo-item-input").val("");
     })
 );
 
 $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_completed container
+
+        // grab item to move to top of completed list
+        $(this).html("Add to To Do");
+
+        var completedItem = $(this).parent()
+        $("#actualcompleted").prepend(completedItem);
 });
 
 $("#list_completed").on('click', "button", function() {
         // move back from list_completed container to list_todo container
+
+        $(this).html("Add to Completed List");
+        var toDoItem = $(this).parent()
+        $("#todolistlist").prepend(toDoItem);
 });
